26735b494c83c8c321d7a061ee5515dd61e31459
Quddus1916 <quddusjunior1916@gmail.com>
2021-05-21 11:52:18 -0500

    practice 2

---
+++ practice_2/string_var.js
@@ -0,0 +1,12 @@
+var username ='molly';
+var greetings ='hiii';
+var message ='See our upcomming range';
+
+var elname = document.getElementById('name');
+elname.textContent= username;
+
+var elnote = document.getElementById('note');
+elnote.textContent = message;
+
+var elname = document.getElementById('title');
+elname.textContent= greetings;
