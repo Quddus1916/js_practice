dca17cd85cfea0dc07c2c9932dace53c72430ee4
Quddus1916 <quddusjunior1916@gmail.com>
2021-06-05 21:59:38 -0500

    task: change the all hot classes to cool class(using loop)

--- document/document.js
+++ document/document.js
@@ -1,3 +1,25 @@
 
-var el = document.getElementById('one');
-el.className = 'cool';
+//var el = document.getElementById('one');
+//el.className = 'cool';
+
+
+// var element = document.getElementsByClassName('hot');
+// if(element.length>1)
+// {
+//     var first_item = element.item[0];
+// }
+
+// var elements = document.getElementsByClassName('hot');
+// if(elements.length>1){
+//     var el3 = elements[2];
+//     el3.className = 'cool';
+// }
+
+var hotelements = document.querySelectorAll('li.hot');
+if(hotelements.length>1)
+{
+    for( i=0;i<hotelements.length;i++)
+    {
+        hotelements[i].className='cool';
+    }
+}
