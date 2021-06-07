53027bd5895f0249e81fe2bd45e85cd36953f785
Quddus1916 <quddusjunior1916@gmail.com>
2021-06-06 21:51:14 -0500

    DOM manipulation

--- document/document.js
+++ document/document.js
@@ -15,11 +15,38 @@
 //     el3.className = 'cool';
 // }
 
-var hotelements = document.querySelectorAll('li.hot');
-if(hotelements.length>1)
-{
-    for( i=0;i<hotelements.length;i++)
-    {
-        hotelements[i].className='cool';
-    }
-}
+// var hotelements = document.querySelectorAll('li.hot');
+// if(hotelements.length>1)
+// {
+//     for( i=0;i<hotelements.length;i++)
+//     {
+//         hotelements[i].className='cool';
+//     }
+// }
+
+// Dom tree manupulation:::::::::::::::element added to first and last position
+
+var list = document.getElementsByTagName('ul')[0];
+
+var last_element = document.createElement('li');
+var last_Text = document.createTextNode('cream');
+last_element.appendChild(last_Text);
+list.appendChild(last_element);
+
+
+var first_element = document.createElement('li');
+var first_text = document.createTextNode('mango');
+first_element.appendChild(first_text);
+list.insertBefore(first_element,list.firstChild);
+
+var el1 = document.getElementsByTagName('li').item(0);
+el1.setAttribute('class','hot');
+
+var el2 = document.getElementsByTagName('li').item(5);
+el2.setAttribute('class' ,'cool');
+
+
+
+
+
+
