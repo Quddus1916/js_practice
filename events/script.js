2f2f537f058572003076123288feacbd3bb8bb94
Quddus1916 <quddusjunior1916@gmail.com>
2021-07-07 02:09:19 -0500

    practice: create a counter to count the letters from a text(input)

--- events/script.js
+++ events/script.js
@@ -20,17 +20,29 @@
 // //el.onblur = checkusername;
 // el.addEventListener('blur',checkusername,false)
 
-function itemdone(e)
-{
-    var target = e.target
-    var elparent = target.parentNode
-    var elgrandparent= target.parentNode.parentNode
-    elgrandparent.removeChild(elparent)
-    e.preventDefault()
+// function itemdone(e)
+// {
+//     var target = e.target
+//     var elparent = target.parentNode
+//     var elgrandparent= target.parentNode.parentNode
+//     elgrandparent.removeChild(elparent)
+//     e.preventDefault()
+
+// }
+
+
+// var el = document.getElementById('shoppinglist')
+//  el.addEventListener('click',function(e){itemdone(e)},false)
+
+function char_count(e){
+    var textentered = document.getElementById('message').value;
+    var chardisplay = document.getElementById('charleft');
+    var counter =(textentered.length);
+    chardisplay.textContent = counter+1;
+    
+  
 
 }
 
-
-var el = document.getElementById('shoppinglist')
- el.addEventListener('click',function(e){itemdone(e)},false)
-
+var el = document.getElementById('message')
+el.addEventListener('keypress',char_count,false)
