b8aa0c9bd583616cd0ff8ae01886c11c85f9975a
Quddus1916 <quddusjunior1916@gmail.com>
2021-07-07 01:46:43 -0500

    practice : delete the element that was clicked from the list

--- events/script.js
+++ events/script.js
@@ -2,20 +2,35 @@
 
 
 
-function checkusername(){
-    var feedback = document.getElementById('feedback');
-    var name = document.getElementById('username');
-    if(name.value.length<5)
-    {
-        feedback.textContent='username must be above 5 characters'
+// function checkusername(){
+//     var feedback = document.getElementById('feedback');
+//     var name = document.getElementById('username');
+//     if(name.value.length<5)
+//     {
+//         feedback.textContent='username must be above 5 characters'
 
-    }
-    else
-    {
-        feedback.textContent=''
-    }
+//     }
+//     else
+//     {
+//         feedback.textContent=''
+//     }
+// }
+
+// var el = document.getElementById('username');
+// //el.onblur = checkusername;
+// el.addEventListener('blur',checkusername,false)
+
+function itemdone(e)
+{
+    var target = e.target
+    var elparent = target.parentNode
+    var elgrandparent= target.parentNode.parentNode
+    elgrandparent.removeChild(elparent)
+    e.preventDefault()
+
 }
 
-var el = document.getElementById('username');
-//el.onblur = checkusername;
-el.addEventListener('blur',checkusername,false)
+
+var el = document.getElementById('shoppinglist')
+ el.addEventListener('click',function(e){itemdone(e)},false)
+
