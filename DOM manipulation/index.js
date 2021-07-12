987a3c5ebc1dd507ad346f5c90f412ed5e569a5a
Quddus1916 <quddusjunior1916@gmail.com>
2021-07-11 22:45:13 -0500

    nodes,sibling,child

--- DOM manipulation/index.js
+++ DOM manipulation/index.js
@@ -1,20 +1,37 @@
 // var el = document.getElementsByClassName('title')
 // Array.from(el).forEach(function(e){ console.log(e)})
 
-const listelement = document.querySelector('#book-list li:nth-child(1) .name')
+//const listelement = document.querySelector('#book-list li:nth-child(1) .name')
 //console.log(listelement)
 
-const allelements = document.querySelectorAll('#book-list li .name');
+//const allelements = document.querySelectorAll('#book-list li .name');
 //console.log(allelements); //just show only one element bt var contains collection
 
-Array.from(allelements).forEach(function(book){
-    console.log(book.textContent)
-})
+// Array.from(allelements).forEach(function(book){
+//     console.log(book.textContent)
+// })
 
-var change = document.querySelector('#book-list li:nth-child(1) .name')
-change.textContent ='changed name'
+// var change = document.querySelector('#book-list li:nth-child(1) .name')
+// change.textContent ='changed name'
 
 
-const value = document.querySelector('#book-list');
+//const value = document.querySelector('#book-list');
 //console.log(allelements); //just show only one element bt var contains collection
-value.innerHTML+='<h2> these are book titles</h2>'
+//value.innerHTML+='<h2> these are book titles</h2>'
+
+
+const banner = document.querySelector('#page-banner')
+console.log('#page-banner node type is:', banner.nodeType)
+console.log('#page-banner node name is:', banner.nodeName)
+console.log('#page-banner node child is:', banner.hasChildNodes())
+
+
+
+const clonedbannner= banner.cloneNode(true)
+console.log(clonedbannner)
+
+
+
+const booklist = document.querySelector('#book-list')
+
+booklist.previousElementSibling.querySelector('p').innerHTML+='<br/> enjoy with books'
